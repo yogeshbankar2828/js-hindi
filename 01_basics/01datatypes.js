bd8af73d47cb34bc4a69e6cc5d9739a8acf9f45b
yogeshbankar2828 <bankary71@gmail.com>
2025-07-25 08:45:45 +0000

    datatypes

---
+++ 01_basics/01datatypes.js
@@ -0,0 +1,13 @@
+"use strict";//treat code as new version of js
+//alert("shreyansh") using node.js not browser
+
+//typeof data 
+/*number 
+string
+null
+undefined
+bolleen
+symbol
+bigint 
+/*
+//obejects
---
+++ 01_basics/01var.js
@@ -0,0 +1,7 @@
+const accountId = 144553
+let accountEmail = "bankary71@gmail.com"
+var accountPassward="12345"
+accountCity= "buldhana"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassward, accountCity])
